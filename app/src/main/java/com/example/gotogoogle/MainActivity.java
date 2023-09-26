f27b8404d848160bccd4d6d3dc0381c08114b85e
jneira1234 <juan.neira052@pascualbravo.edu.co>
2023-09-25 20:19:59 -0500

    goTogoogle

--- app/src/main/java/com/example/gotogoogle/MainActivity.java
+++ app/src/main/java/com/example/gotogoogle/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.gotogoogle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button google, alarma, llamada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        google = findViewById(R.id.google);
+        alarma = findViewById(R.id.alarma);
+        llamada = findViewById(R.id.llamada);
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent irGoogle = new Intent(Intent.ACTION_VIEW);
+                irGoogle.setData(Uri.parse("http:/www.google.com"));
+                startActivity(irGoogle);
+            }
+        });
+
+        alarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                crearAlarma();
+            }
+        });
+
+        llamada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+
+    public void crearAlarma(){
+
+        Intent alarma = new Intent(AlarmClock.ACTION_SET_ALARM);
+        alarma.putExtra(AlarmClock.EXTRA_MESSAGE, "Gignacio");
+        alarma.putExtra(AlarmClock.EXTRA_HOUR, 10);
+        alarma.putExtra(AlarmClock.EXTRA_MINUTES, 30);
+    if (alarma.resolveActivity(getPackageManager()) != null) {
+        startActivity(alarma);
+    }
     }
 }
